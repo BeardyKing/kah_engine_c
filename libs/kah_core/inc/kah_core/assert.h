f39bacc818e0e05c01f774520dfdca643a7dfcd4
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2025-08-10 02:14:19 +0100

    [CORE] changed debug break location to make assert call stack cleaner
    I moved the debug break into the core assert functions because of implicit printfs but this made debugbreak break in the assert function not at the location the assert was triggered, so I've moved it out into the macro instead.

--- libs/kah_core/inc/kah_core/assert.h
+++ libs/kah_core/inc/kah_core/assert.h
@@ -1,22 +1,22 @@
 #ifndef ASSERT_H
 #define ASSERT_H
 
-//===INCLUES======================================================================
+//===INCLUDES==================================================================
 #include <kah_core/defines.h>
 
 #include <stdbool.h>
 //=============================================================================
 
 //===IMPL======================================================================
-void _core_assert_impl(bool condition, const char* file, const char* function, int line);
-void _core_assert_msg_impl(bool condition, const char* file, const char* function, int line, const char* fmt, ...);
-void _core_not_implemented_impl(const char* file, const char* function, int line);
+bool _core_assert_impl(bool condition, const char* file, const char* function, int line);
+bool _core_assert_msg_impl(bool condition, const char* file, const char* function, int line, const char* fmt, ...);
+bool _core_not_implemented_impl(const char* file, const char* function, int line);
 //=============================================================================
 
 //===API=======================================================================
-#define core_assert(condition)          _core_assert_impl((condition), __FILE__, __FUNCTION__, __LINE__)
-#define core_assert_msg(condition, ...) _core_assert_msg_impl((condition), __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)
-#define core_not_implemented()          _core_not_implemented_impl(__FILE__, __FUNCTION__, __LINE__)
+#define core_assert(condition)          if (!_core_assert_impl((condition), __FILE__, __FUNCTION__, __LINE__)) core_debug_break()
+#define core_assert_msg(condition, ...) if (!_core_assert_msg_impl((condition), __FILE__, __FUNCTION__, __LINE__, __VA_ARGS__)) core_debug_break()
+#define core_not_implemented()          if (!_core_not_implemented_impl(__FILE__, __FUNCTION__, __LINE__)) core_debug_break()
 
 #if defined(__linux__) || defined(__APPLE__)
 #include <csignal>
@@ -26,7 +26,5 @@ void _core_not_implemented_impl(const char* file, const char* function, int line
 #else
 #define core_debug_break() // not implemented
 #endif
-
 //=============================================================================
-
 #endif //ASSERT_H
--- libs/kah_core/src/assert.c
+++ libs/kah_core/src/assert.c
@@ -1,11 +1,12 @@
-//===INCLUES======================================================================
+//===INCLUDES==================================================================
 #include <kah_core/assert.h>
 
 #include <stdarg.h>
 #include <stdio.h>
-//================================================================================
+#include <stdbool.h>
+//=============================================================================
 
-//===INTERNAL_FUNCTIONS===========================================================
+//===INTERNAL_FUNCTIONS=======================================================
 static void internal_core_print_error(const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
@@ -16,30 +17,32 @@ static void internal_core_print_error(const char* fmt, ...) {
 static void internal_core_log_current_position(const char* file, const char* function, int line) {
     internal_core_print_error("Assertion triggered \n\t file: \t %s \n\t func: \t %s \n\t line: \t %d \n", file, function, line);
 }
-//================================================================================
+//=============================================================================
 
-//===API==========================================================================
-void _core_assert_impl(bool condition, const char* file, const char* function, int line) {
+//===API=======================================================================
+bool _core_assert_impl(bool condition, const char* file, const char* function, int line) {
     if (!condition) {
         internal_core_log_current_position(file, function, line);
-        core_debug_break();
+        return false;
     }
+    return true;
 }
 
-void _core_assert_msg_impl(bool condition, const char* file, const char* function, int line, const char* fmt, ...) {
+bool _core_assert_msg_impl(bool condition, const char* file, const char* function, int line, const char* fmt, ...) {
     if (!condition) {
         internal_core_log_current_position(file, function, line);
         va_list args;
         va_start(args, fmt);
         vprintf(fmt, args);
         va_end(args);
-        core_debug_break();
+        return false;
     }
+    return true;
 }
 
-void _core_not_implemented_impl(const char* file, const char* function, int line) {
+bool _core_not_implemented_impl(const char* file, const char* function, int line) {
     internal_core_log_current_position(file, function, line);
     internal_core_print_error("Hit unimplemented code o_o\n");
-    core_debug_break();
+    return false;
 }
-//================================================================================
+//=============================================================================

1821a6eefc27c5f9e0effae54f7dfa17bfe7db1d
BeardyKing <39307332+BeardyKing@users.noreply.github.com>
2025-11-18 23:23:57 +0000

    [CLIENT] Removed run test from main
    removed tests from game exe & set the window size on the window_create func instead of passing it in.
    
    once I have some kind of CI I will add a exe that just runs all of the tests for the core data structures.

--- projects/kah_game/main.c
+++ projects/kah_game/main.c
@@ -8,10 +8,6 @@
 #include <kah_core/time.h>
 #include <kah_core/window.h>
 #include <kah_core/bit_array.h>
-#if CHECK_FEATURE(FEATURE_RUN_TESTS)
-#include <kah_core/tests/test_bits.h>
-#include <kah_core/tests/test_data_structures.h>
-#endif
 
 #include <kah_gfx/gfx_interface.h>
 #include <kah_gfx/vulkan/gfx_vulkan_imgui.h>
@@ -19,24 +15,6 @@
 #include <stdint.h>
 #include <string.h>
 //=============================================================================
-
-void test_run_all(){
-#if CHECK_FEATURE(FEATURE_RUN_TESTS)
-    //TODO: setup project that just runs tests for various libs
-    mem_create();
-    allocator_create();
-    {
-
-        test_run_bitset();
-        test_run_bitarray();
-
-        test_run_data_structures();
-    }
-    mem_cleanup();
-    allocator_cleanup();
-    core_assert_msg(mem_alloc_table_empty(), "err: memory leaks");
-#endif
-}
 
 
 #if CHECK_FEATURE(FEATURE_GFX_IMGUI)
@@ -51,14 +29,10 @@ void imgui_update() {
 
 int main(void)
 {
-    test_run_all();
-
-    const vec2i windowSize = {1024, 768};
     mem_create();
     allocator_create();
-
     {
-        window_create("kah engine - runtime", windowSize, KAH_WINDOW_POSITION_CENTERED);
+        window_create("kah engine - runtime", (vec2i){1024, 768}, KAH_WINDOW_POSITION_CENTERED);
 #if CHECK_FEATURE(FEATURE_GFX_IMGUI)
         window_set_procedure_callback_func(gfx_imgui_get_proc_function_pointer());
 #endif //CHECK_FEATURE(FEATURE_GFX_IMGUI)
